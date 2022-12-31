4cad85888bd3cb923a831654759449e041914332
Joseph Innes <joseph.innes@swissinfo.ch>
2022-12-31 08:40:40 +0100

    Add landing page

---
+++ data.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func handleGetData(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "public, max-age=604800, immutable")
+
+	fileHash := strings.TrimPrefix(r.URL.Path, "/")
+	if fileHash == "" {
+		http.Error(w, "missing File-Hash in path", http.StatusBadRequest)
+		return
+	}
+
+	qRes, err := makeQuery(fileHash)
+	if err != nil {
+		werr := fmt.Sprintf("failed to query Arweave: %s", err)
+		http.Error(w, werr, http.StatusInternalServerError)
+		log.Println(werr)
+		return
+	}
+
+	if len(qRes.Data.Transactions.Edges) != 1 {
+		http.Error(w, "nothing found for given File-Hash", http.StatusNotFound)
+		return
+	}
+	node := qRes.Data.Transactions.Edges[0].Node
+	contentType, err := getContentType(node.Tags)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("unable to get Content-Type from tags: %s", err.Error())
+		return
+	}
+	dataUrl := ""
+	if contentType == "text/plain" {
+		dataUrl = fmt.Sprintf("http://arweave.net/tx/%s/data", node.Id)
+	} else {
+		typeSegments := splitMimeType(contentType)
+		subtype := ""
+		if len(typeSegments) == 1 {
+			subtype = typeSegments[0]
+		} else if len(typeSegments) >= 2 {
+			subtype = typeSegments[1]
+		} else {
+			http.Error(w, fmt.Sprintf("unable to handle Content-Type: %s", contentType), http.StatusInternalServerError)
+			return
+		}
+		dataUrl = fmt.Sprintf("http://arweave.net/tx/%s/data.%s", node.Id, subtype)
+	}
+	resp, err := http.Get(dataUrl)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("failed to get transaction data: %s", err), http.StatusInternalServerError)
+		return
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	w.Header().Set("Content-Type", contentType)
+	w.Header().Set("Content-Length", strconv.Itoa(buf.Len()))
+	w.Write(buf.Bytes())
+}
+
+func getContentType(tags []Tag) (string, error) {
+	for _, t := range tags {
+		if t.Name == "Content-Type" {
+			return t.Value, nil
+		}
+	}
+	return "", errors.New("no Content-Type tag found")
+}
+
+func splitMimeType(mimeType string) []string {
+	return strings.Split(mimeType, "/")
+}
--- main.go
+++ main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			handleGet(w, r)
-		}
-	})
+	http.HandleFunc("/", handleRoot)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "1992"
@@ -26,67 +18,20 @@ func main() {
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-func handleGet(w http.ResponseWriter, r *http.Request) {
-	fileHash := strings.TrimPrefix(r.URL.Path, "/")
-	if fileHash == "" {
-		http.Error(w, "missing File-Hash in path", http.StatusBadRequest)
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "method not supported", http.StatusBadRequest)
 		return
 	}
-	qRes, err := makeQuery(fileHash)
-	if err != nil {
-		werr := fmt.Sprintf("failed to query Arweave: %s", err)
-		http.Error(w, werr, http.StatusInternalServerError)
-		log.Println(werr)
+	// set default cache header
+	w.Header().Set("Cache-Control", "public, max-age=3600")
+	if r.URL.Path == "/" {
+		http.ServeFile(w, r, "landing/index.html")
 		return
 	}
-	if len(qRes.Data.Transactions.Edges) != 1 {
-		http.Error(w, "nothing found for given File-Hash", http.StatusNotFound)
+	if r.URL.Path == "/styles.css" {
+		http.ServeFile(w, r, "landing/styles.css")
 		return
 	}
-	node := qRes.Data.Transactions.Edges[0].Node
-	contentType, err := getContentType(node.Tags)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	dataUrl := ""
-	if contentType == "text/plain" {
-		dataUrl = fmt.Sprintf("http://arweave.net/tx/%s/data", node.Id)
-	} else {
-		typeSegments := splitMimeType(contentType)
-		subtype := ""
-		if len(typeSegments) == 1 {
-			subtype = typeSegments[0]
-		} else if len(typeSegments) >= 2 {
-			subtype = typeSegments[1]
-		} else {
-			http.Error(w, fmt.Sprintf("unable to handle Content-Type: %s", contentType), http.StatusInternalServerError)
-			return
-		}
-		dataUrl = fmt.Sprintf("http://arweave.net/tx/%s/data.%s", node.Id, subtype)
-	}
-	resp, err := http.Get(dataUrl)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("failed to get transaction data: %s", err), http.StatusInternalServerError)
-		return
-	}
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	w.Header().Add("Content-Type", contentType)
-	w.Header().Add("Content-Length", strconv.Itoa(buf.Len()))
-	w.Header().Add("Cache-Control", "public, max-age=604800, immutable")
-	w.Write(buf.Bytes())
-}
-
-func getContentType(tags []Tag) (string, error) {
-	for _, t := range tags {
-		if t.Name == "Content-Type" {
-			return t.Value, nil
-		}
-	}
-	return "", errors.New("no Content-Type tag found")
-}
-
-func splitMimeType(mimeType string) []string {
-	return strings.Split(mimeType, "/")
+	handleGetData(w, r)
 }
